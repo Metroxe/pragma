ac446dcecdfa37da0dc5bd6c322dfbeac4ba5e41
Spencer Gray <spencergray666@hotmail.com>
2019-02-16 10:01:05 -0800

    added linear gradient library

--- src/containers/Container.tsx
+++ src/containers/Container.tsx
@@ -6,81 +6,90 @@ import {IGameData} from "../services/GameData";
 import {IGameFunctions} from "../services/GameFunctions";
 import {IPagePackager, TabNavigator} from "../components/TabNavigator";
 import {Header} from "../components/Header";
+import LinearGradient from 'react-native-linear-gradient';
 
 export default class Container<P extends IContainerProps, S extends IContainerState> extends React.PureComponent<P, S> {
 
-    private static containerStyle: StyleSheet.NamedStyles<IStyle> = StyleSheet.create<IStyle>({
-        topView: {
-            backgroundColor: "white",
-            // flex: 1,
-            height: Dimensions.get("screen").height,
-            width: Dimensions.get("screen").width,
-        },
-    });
+	private static containerStyle: StyleSheet.NamedStyles<IStyle> = StyleSheet.create<IStyle>({
+		topView: {
+			backgroundColor: "white",
+			// flex: 1,
+			height: Dimensions.get("screen").height,
+			width: Dimensions.get("screen").width,
+		},
+		popUpModalContainer: {},
+	});
 
-    private static pagesArray: IPagePackager[] = [
-        {
-            pageString: "Grid",
-            displayString: "Map",
-        },
-        {
-            pageString: "AllocatePeopleScreen",
-            displayString: "Allocate",
-        },
-        {
-            pageString: "ShopScreen",
-            displayString: "ShopScreen",
-        },
-    ];
+	private static pagesArray: IPagePackager[] = [
+		{
+			pageString: "Grid",
+			displayString: "Map",
+		},
+		{
+			pageString: "AllocatePeopleScreen",
+			displayString: "Allocate",
+		},
+		{
+			pageString: "ShopScreen",
+			displayString: "ShopScreen",
+		},
+	];
 
-    public renderPointer: () => ReactNode;
+	public renderPointer: () => ReactNode;
 
-    constructor(props: P) {
-        super(props);
+	constructor(props: P) {
+		super(props);
 
-        // @ts-ignore
-        this.state = {};
-        this.wrapRender = this.wrapRender.bind(this);
-        this.wrapRender();
-    }
+		// @ts-ignore
+		this.state = {};
+		this.wrapRender = this.wrapRender.bind(this);
+		this.wrapRender();
+	}
 
-    public wrapRender(): void {
-        this.renderPointer = this.render;
-        this.render = (): ReactNode => {
-            return (
-                <View style={Container.containerStyle.topView}>
-                    <Header
-                        title={Container.pagesArray[Container.pagesArray.findIndex((property: IPagePackager) => property.pageString === this.props.currentPage.toString())].displayString}
-                    />
+	public wrapRender(): void {
+		this.renderPointer = this.render;
+		this.render = (): ReactNode => {
+			return (
+				<View style={Container.containerStyle.topView}>
 
-                    <View
-                        style={{height: Dimensions.get("screen").height - Header.headerHeight - TabNavigator.navBarHeight}}>
-                        {this.renderPointer()}
-                    </View>
+					{this.state.popUpModalContent &&
+					<View style={Container.containerStyle.popUpModalContainer}>
+						{this.state.popUpModalContent}
+					</View>
+					}
 
-                    <TabNavigator
-                        tabOptions={Container.pagesArray}
-                        navigate={this.props.navigate}
-                    />
-                </View>
-            );
-        };
-    }
+					<Header
+						title={Container.pagesArray[Container.pagesArray.findIndex((property: IPagePackager) => property.pageString === this.props.currentPage.toString())].displayString}
+					/>
+
+					<View
+						style={{height: Dimensions.get("screen").height - Header.headerHeight - TabNavigator.navBarHeight}}>
+						{this.renderPointer()}
+					</View>
+
+					<TabNavigator
+						tabOptions={Container.pagesArray}
+						navigate={this.props.navigate}
+					/>
+				</View>
+			);
+		};
+	}
 
 }
 
 interface IStyle {
-    topView: ViewStyle;
-
+	topView: ViewStyle;
+	popUpModalContainer: ViewStyle;
 }
 
 export interface IContainerProps {
-    navigate: (page: keyof IContainerSet) => Promise<void>;
-    gameData: IGameData;
-    gameFunctions: IGameFunctions;
-    currentPage: keyof IContainerSet;
+	navigate: (page: keyof IContainerSet) => Promise<void>;
+	gameData: IGameData;
+	gameFunctions: IGameFunctions;
+	currentPage: keyof IContainerSet;
 }
 
 export interface IContainerState {
-
+	popUpModalContent: ReactNode;
 }
