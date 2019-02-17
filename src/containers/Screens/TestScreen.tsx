6f6c52765dd88b71259482a8c6bd9bea3ce05be2
Spencer Gray <spencergray666@hotmail.com>
2019-02-17 01:54:27 -0800

    did the thing with the components for allocating people to do work

--- src/containers/Screens/TestScreen.tsx
+++ src/containers/Screens/TestScreen.tsx
@@ -3,8 +3,9 @@ import {ReactNode} from "react";
 import * as React from "react";
 import {View, Text} from "react-native";
 import {TabNavigator} from "../../components/TabNavigator";
-import ShopItemComponent from "../../components/ShopItemComponent";
-import ShopComponentList from "../../components/ShopComponentList";
+import ShopComponentItem from "../../components/ShopAndPeopleAllocation/ShopComponentItem";
+import ShopComponentItemList from "../../components/ShopAndPeopleAllocation/ShopComponentItemList";
+import PeopleAllocationItemList from "../../components/ShopAndPeopleAllocation/PeopleAllocationItemList";
 
 export default class TestScreen extends Container<ITestScreenProps, ITestScreenState> {
 	public render(): ReactNode {
@@ -12,7 +13,7 @@ export default class TestScreen extends Container<ITestScreenProps, ITestScreenS
 			<View>
 				{/*<Text>{JSON.stringify(this.props.gameData, null, 2)}</Text>*/}
 				{/*<Text>{this.props.gameData.time}</Text>*/}
-				<ShopComponentList/>
+				<PeopleAllocationItemList/>
 			</View>
 		);
 	}
