858cc44a5263b2bfcfc88ca545a82dd44d36a695
raymondchiu10 <raymondchiu10@gmail.com>
2019-02-17 12:12:47 -0800

    Revert "Default building values"

--- android/app/src/main/java/com/baymax/MainApplication.java
+++ android/app/src/main/java/com/baymax/MainApplication.java
@@ -3,13 +3,10 @@ package com.baymax;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.zmxv.RNSound.RNSoundPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.BV.LinearGradient.LinearGradientPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,9 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-              new RNSoundPackage(),
-          new LinearGradientPackage()
+          new MainReactPackage()
       );
     }
 
